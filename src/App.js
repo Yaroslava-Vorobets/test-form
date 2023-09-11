4585e849e24f50079631cd92697e7f2dc0214e0a
Yaroslava-Vorobets <slavavor85@gmail.com>
2023-09-11 12:33:00 +0200

    change the title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          do geo location
         </a>
       </header>
     </div>
