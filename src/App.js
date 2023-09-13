629470b0c5d4fadf8b45c3c1b6d48722b853dc2a
Yaroslava-Vorobets <slavavor85@gmail.com>
2023-09-13 16:35:46 +0200

    added packages: react-google-maps use-places-autocomplete react-cool-onclickoutside

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+const API_KEY = process.env.REACT_APP_API_KEY
+console.log(API_KEY)
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          do geo location
-        </a>
-      </header>
-    </div>
-  );
+export const App = () => {
+
+    return (
+        <div>
+            <h1>hello googleMaps</h1> 
+          </div>
+     )
+    
 }
 
-export default App;
